cb993076da245b962ad807f1df7a874f8c65a526
Simon Schubert <simon@librem.one>
2024-12-30 12:31:58 +0100

    Add TextFieldValue rememberSaveable

--- composeApp/src/commonMain/kotlin/com/inspiredandroid/kai/ui/chat/ChatScreen.kt
+++ composeApp/src/commonMain/kotlin/com/inspiredandroid/kai/ui/chat/ChatScreen.kt
@@ -42,6 +42,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -296,7 +297,7 @@ private fun EmptyState(modifier: Modifier, isUsingSharedKey: Boolean) {
 private fun QuestionInput(ask: (String) -> Unit) {
     val focusManager = LocalFocusManager.current
 
-    var textState by remember { mutableStateOf(TextFieldValue("")) }
+    var textState by rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue("")) }
 
     fun submitQuestion() {
         val text = textState.text
