5f8da380331d5c613c4534813a7af31f4c85f5ed
Simon Schubert <simon@librem.one>
2026-01-24 12:52:03 +0100

    Add ios tts

--- composeApp/src/iosMain/kotlin/com/inspiredandroid/kai/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/inspiredandroid/kai/MainViewController.kt
@@ -2,10 +2,14 @@ package com.inspiredandroid.kai
 
 import androidx.compose.ui.window.ComposeUIViewController
 import androidx.navigation.compose.rememberNavController
+import nl.marc_apps.tts.TextToSpeechEngine
+import nl.marc_apps.tts.rememberTextToSpeechOrNull
 
 fun MainViewController() = ComposeUIViewController {
     val navController = rememberNavController()
+    val textToSpeech = rememberTextToSpeechOrNull(TextToSpeechEngine.SystemDefault)
     App(
         navController = navController,
+        textToSpeech = textToSpeech,
     )
 }
