b12dde907ae1012f22ef5fd7f7a9a013c87b1c5d
Simon Schubert <simon@librem.one>
2026-01-27 14:35:12 +0100

    Use common preview

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -61,7 +61,6 @@ kotlin {
         }
 
         androidMain.dependencies {
-            implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.android)
             implementation(libs.koin.android)
