01e087a7375755a514aa4065518d5867f316dc9d
Simon Schubert <simon@librem.one>
2026-02-11 17:21:48 +0100

    Add missing proguard rules

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -177,6 +177,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
     }
