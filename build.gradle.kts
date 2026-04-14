d25d113985301b2f72bc1b2a09b0ec3f5332e9f3
Simon Schubert <simon@librem.one>
2026-04-14 13:08:45 +0200

    Upgrade bouncycastle and remove version checker dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -10,7 +8,6 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.spotless)
-    alias(libs.plugins.ben.manes.versions)
 }
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
@@ -33,17 +30,4 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         target("**/*.gradle.kts")
         ktlint()
     }
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
 }
